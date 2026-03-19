0af7df76eaf0e3fed7ea1ee94d2aee64ec1322e5
Fred120-code <fredayemtsa@gmail.com>
2026-03-19 02:02:34 +0100

    initalation de prisma

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-
+import { clerkMiddleware } from "@clerk/express";
+import { prisma } from "./db";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
   res.send("Server in runnig");
@@ -16,3 +18,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
