eba8a7c6c28223cb7655d7ed42ef39f9a8899b33
Fred120-code <fredayemtsa@gmail.com>
2026-03-25 22:23:13 +0100

    feat: start project page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,8 +1,47 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Plus, Search, FolderOpen } from "lucide-react";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+  const projects = useSelector(
+    (state) => state?.workspace?.currentWorspace || [],
+  );
+  const [filteredProjects, setFilteredProjects] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [filter, setFilter] = useState({
+    status: "ALL",
+    priority: "ALL",
+  });
 
-export default Projects
+  const filterProjects = () => {
+    let filtered = projects;
+
+    if (searchTerm) {
+      filtered = filtered.filter(
+        (project) =>
+          project.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          project.description?.toLowerCase().includes(searchTerm.toLowerCase()),
+      );
+    }
+    if (filter.status !== "ALL") {
+      filtered = filtered.filter((project) => project.status === filter.status);
+    }
+
+    if (filter.priorIty !== "ALL") {
+      filtered = filtered.filter(
+        (project) => project.priority === filter.priority,
+      );
+    }
+
+    setFilteredProjects(filtered);
+  };
+
+  useEffect(() => {
+    filterProjects();
+  }, [projects, filter, searchTerm]);
+
+  return <div></div>;
+};
+
+export default Projects;
