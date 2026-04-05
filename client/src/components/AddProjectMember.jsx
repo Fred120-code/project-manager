94548650ccc2e2cb2769a6713381aa64a2e8b292
Fred120-code <fredayemtsa@gmail.com>
2026-04-05 17:56:25 +0100

    fix:add func in AddProjectMember comp

--- client/src/components/AddProjectMember.jsx
+++ client/src/components/AddProjectMember.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import { Mail, UserPlus } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 
 const AddProjectMember = ({ isDialogOpen, setIsDialogOpen }) => {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+
+  const currentWorkspace = useSelector(
+    (state) => state.workspace?.currentWorkspace || null,
+  );
+
+  const project = currentWorkspace?.projects.find((p) => p.id === id);
+  const projectMembersEmails = project?.members.map(
+    (member) => member.user.email,
+  );
+
+  const [email, setEmail] = useState("");
+  const [isAdding, setIsAdding] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  if (!isDialogOpen) return null;
   return <div>AddProjectMember</div>;
 };
 
