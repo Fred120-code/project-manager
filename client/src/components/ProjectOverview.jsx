3c090500c35be5e9a3eda3b747e39782dc4b978b
Fred120-code <fredayemtsa@gmail.com>
2026-04-07 02:36:20 +0100

    fix: add project overview

--- client/src/components/ProjectOverview.jsx
+++ client/src/components/ProjectOverview.jsx
@@ -32,8 +32,89 @@ const ProjectOverview = () => {
   useEffect(() => {
     setProjects(currentWorkspace?.projects || []);
   }, [currentWorkspace]);
-  
-  return <div>ProjectOverview</div>;
+
+  return (
+    currentWorkspace && (
+      <div
+        className="bg-white dark:bg-zinc-950 dark:bg-gradient-to-br dark:from-zinc-800/70
+         dark:to-zinc-900/50 border border-zinc-200 dark:border-zinc-800 hover:border-zinc-300
+          dark:hover:border-zinc-700 transition-all duration-200 rounded-lg overflow-hidden"
+      >
+        <div className="border-b border-zinc-200 dark:border-zinc-800 p-4 flex items-center justify-between">
+          <h2 className="text-md text-zinc-800 dark:text-zinc-300">
+            Project Overview
+          </h2>
+          <Link
+            to={"/projects"}
+            className="flex items-center text-sm text-zinc-600 hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300"
+          >
+            View all <ArrowRight className="w-4 h-4 ml-2" />
+          </Link>
+        </div>
+
+        <div className="p-0">
+          {projects.length === 0 ? (
+            <div className="p-12 text-center">
+              <div
+                className="w-16 h-16 mx-auto mb-4 bg-zinc-200 text-zinc-600 dark:bg-zinc-800
+                         dark:text-zinc-500 rounded-full flex items-center justify-center"
+              >
+                <FolderOpen size={32} />
+              </div>
+              <p className="text-zinc-600 dark:text-zinc-400">
+                No projects yet
+              </p>
+              <button
+                onClick={() => setIsDialogOpen(true)}
+                className="mt-4 px-4 py-2 text-sm bg-gradient-to-br from-blue-500 to-blue-600 text-white
+                             dark:text-zinc-200 rounded hover:opacity-90 transition"
+              >
+                Create your First Project
+              </button>
+              <CreateProjectDialog
+                isDialogOpen={isDialogOpen}
+                setIsDialogOpen={setIsDialogOpen}
+              />
+            </div>
+          ) : (
+            <div className=" divide-y divide-zinc-200 dark:divide-zinc-800">
+              {projects.slice(0, 5).map((project) => (
+                <Link
+                  key={project.id}
+                  to={`/projectsDetail?id=${project.id}&tab=tasks`}
+                  className="block p-6 hover:bg-zinc-50 dark:hover:bg-zinc-900/50 transition-colors"
+                >
+                  <div className="flex items-start justify-between mb-3">
+                    <div className="flex-1">
+                      <h3 className="font-semibold text-zinc-800 dark:text-zinc-300 mb-1">
+                        {project.name}
+                      </h3>
+                      <p className="text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2 ">
+                        {project.description || "No description"}
+                      </p>
+                    </div>
+
+                    <div className="flex items-center gap-2 ml-4">
+                      <span
+                        className={`text-xs px-2 py-1 rounded ${statusColors[project.status]}`}
+                      >
+                        {project.status
+                          .replace("_", " ")
+                          .replaceAll(/\b\w/g, (c) => c.toUpperCase())}
+                      </span>
+                      <div
+                        className={`w-2 h-2 rounded-full border-2 ${priorityColors[project.priority]}`}
+                      />
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  );
 };
 
 export default ProjectOverview;
