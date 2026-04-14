3bc213e1746c928e16ee99cadf11c83c6560edbf
Fred120-code <fredayemtsa@gmail.com>
2026-04-14 01:24:55 +0100

    fix: fixed workspaceDropdown

--- client/src/components/WorkspaceDropdown.jsx
+++ client/src/components/WorkspaceDropdown.jsx
@@ -4,8 +4,15 @@ import { setCurrentWorkspace } from "../features/workspaceSlice";
 import { dummyWorkspaces } from "../assets/assets";
 import { useEffect, useRef, useState } from "react";
 import { Check, ChevronDown, Plus } from "lucide-react";
+import { useClerk, useOrganizationList } from "@clerk/react";
 
 const WorkspaceDropdown = () => {
+  const { setActive, userMemberships, isLoaded } = useOrganizationList({
+    userMemberships: true,
+  });
+
+  const { openCreateOrganization } = useClerk();
+
   const { workspaces } = useSelector((state) => state.workspace);
   const currentWorkspace = useSelector(
     (state) => state.workspace?.currentWorkspace || null,
@@ -17,11 +24,13 @@ const WorkspaceDropdown = () => {
   const navigate = useNavigate();
 
   const onSelectWorkspace = (organizationId) => {
+    setActive({ organization: organizationId });
     dispatch(setCurrentWorkspace(organizationId));
     setIsOpen(false);
     navigate("/");
   };
 
+  //Close dropdown on outside click
   useEffect(() => {
     function handleClickOutside(event) {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -31,6 +40,12 @@ const WorkspaceDropdown = () => {
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
+
+  useEffect(() => {
+    if (currentWorkspace && isLoaded) {
+      setActive({ organization: currentWorkspace.id });
+    }
+  }, [currentWorkspace, isLoaded]);
 
   return (
     <div className=" relative m-4" ref={dropdownRef}>
@@ -66,27 +81,27 @@ const WorkspaceDropdown = () => {
             <p className="text-xs text-gray-500 dark:text-zinc-400 uppercase tracking-wider mb-2 px-2">
               Workspaces
             </p>
-            {dummyWorkspaces.map((ws) => (
+            {userMemberships.data.map(({ organization }) => (
               <div
-                key={ws.id}
-                onClick={() => onSelectWorkspace(ws.id)}
+                key={organization.id}
+                onClick={() => onSelectWorkspace(organization.id)}
                 className=" flex items-center gap-3 p-2 cursor-pointer
                        rounded hover:bg-gray-100 dark:hover:bg-zinc-800"
               >
                 <img
-                  src={ws.image_url}
-                  alt={ws.name}
+                  src={organization.imageUrl}
+                  alt={organization.name}
                   className="w-6 h-6 rounded"
                 />
                 <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-800 dark:text-white truncate+">
-                    {ws.name}
+                  <p className="text-sm font-medium text-gray-800 dark:text-white truncate">
+                    {organization.name}
                   </p>
                   <p className="text-xs text-gray-500 dark:text-zinc-400 truncate">
-                    {ws.members.length || 0} members
+                    {organization.membersCount || 0} members
                   </p>
                 </div>
-                {currentWorkspace?.id === ws.id && (
+                {currentWorkspace?.id === organization.id && (
                   <Check className="w-4 h-4 text-blue-600 dark:text-blue-400 shrink-0" />
                 )}
               </div>
@@ -95,7 +110,13 @@ const WorkspaceDropdown = () => {
 
           <hr className="border-gray-200 dark:border-zinc-700" />
 
-          <div className="p-2 cursor-pointer rounded group hover:bg-gray-100 dark:hover:bg-zinc-800">
+          <div
+            onClick={() => {
+              openCreateOrganization();
+              setIsOpen(false);
+            }}
+            className="p-2 cursor-pointer rounded group hover:bg-gray-100 dark:hover:bg-zinc-800"
+          >
             <p
               className="flex items-center text-xs gap-2 my-1 w-full text-blue-600 dark:text-blue-400 
             group-hover:text-blue-500 dark:group-hover:text-blue-300"
